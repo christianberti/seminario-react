76ffb693a2dbe1488095e34c386eb0844a181f97
ezequiel <alcayaga147147@gmail.com>
2026-06-28 22:32:33 -0300

    Panel: mostrar saldo disponible

--- src/pages/panel/PanelPage.jsx
+++ src/pages/panel/PanelPage.jsx
@@ -1,17 +1,21 @@
-import { useEffect, useState } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import api from '../../utils/axiosConfig';
 import '../../assets/styles/Panel.css';
 import { REFRESH_INTERVAL } from '../../utils/constants';
-
+import { AuthContext } from '../../context/AuthContext';
 const PanelPage = () => {
     const [assets, setAssets] = useState([]);
     const [cargando, setCargando] = useState(true);
     const [mensaje, setMensaje] = useState('');
+    const { auth } = useContext(AuthContext);
+    const [dineroDisponible, setDineroDisponible] = useState(0);
 
 
     const cargarAssets = async () => {
         try {
         const response = await api.get('/assets');
+        const userResponse = await api.get(`/users/${auth.userId}`);
+        setDineroDisponible(userResponse.data.data.balance);
         setAssets(response.data.data);
         } catch (error) {
         setMensaje(error.response?.data?.message || 'Error al cargar los assets');
@@ -26,7 +30,7 @@ useEffect(() => {
   const interval = setInterval(cargarAssets, REFRESH_INTERVAL);
 
   return () => clearInterval(interval);
-}, []);
+}, [auth.userId]);
 
   if (cargando) {
     return <p className="panel-loading">Cargando assets...</p>;
@@ -34,10 +38,10 @@ useEffect(() => {
 
   return (
     <main className="panel-container">
-      <h2>Panel</h2>
+    <h2>Panel</h2>
 
-      {mensaje && <p className="panel-error">{mensaje}</p>}
-
+    {mensaje && <p className="panel-error">{mensaje}</p>}
+    <p>Dinero disponible: ${Number(dineroDisponible).toFixed(2)}</p>
       <section className="panel-list">
         {assets.map((asset) => (
           <article className="panel-card" key={asset.id}>
