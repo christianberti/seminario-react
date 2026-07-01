10601e81dee01fbcd26938ce1cf2ab5504e99b10
ezequiel <alcayaga147147@gmail.com>
2026-07-01 20:21:35 -0300

    fix bug f5

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -1,25 +1,14 @@
-import { createContext, useState, useEffect } from 'react';
+import { createContext, useState } from 'react';
 
 export const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
-  const [auth, setAuth] = useState({
-    token: null,
-    name: null,
-    isAdmin: false,
-    userId: null,
-  });
-
-  useEffect(() => {
-    const token = localStorage.getItem('token');
-    const name = localStorage.getItem('name');
-    const isAdmin = localStorage.getItem('isAdmin') === 'true';
-    const userId = localStorage.getItem('userId');
-    
-    if (token) {
-      setAuth({ token, name, isAdmin, userId });
-    }
-  }, []);
+  const [auth, setAuth] = useState(() => ({
+    token: localStorage.getItem('token'),
+    name: localStorage.getItem('name'),
+    isAdmin: localStorage.getItem('isAdmin') === 'true',
+    userId: localStorage.getItem('userId'),
+  }));
 
   const login = (token, name, isAdmin, userId) => {
     localStorage.setItem('token', token);
@@ -27,7 +16,7 @@ export const AuthProvider = ({ children }) => {
     localStorage.setItem('isAdmin', isAdmin);
     localStorage.setItem('userId', userId);
     setAuth({ token, name, isAdmin, userId });
-};
+  };
 
   const logout = () => {
     localStorage.removeItem('token');
